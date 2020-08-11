966a2494d8cadb8e7496decca5aa2117058330ed
bruno alfred <alfredybruno@gmail.com>
2020-08-11 07:54:45 +0300

    changed the path to routes files

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Compress::default())
-            .route("/", web::get().to(routes::req_routes::index))
-            .route("/about", web::get().to(routes::req_routes::index2))
-            .route("/login", web::get().to(routes::req_routes::index3))
-            .route("/register", web::get().to(routes::req_routes::index4))
+            .route("/", web::get().to(routes::home::index))
+            .route("/about", web::get().to(routes::about::index2))
+            .route("/login", web::get().to(routes::login::index3))
+            .route("/register", web::get().to(routes::register::index4))
             .service(routes::req_routes::index5)
     })
     .bind("127.0.0.1:8088")?
